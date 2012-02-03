d43b01c6b061f40e0854eef37a34cb9c1af42d18
Ishu <ishwinder@enbake.com>
2012-02-03 18:45:59 +0530

    First commit for Authorize.net integration with rails 3.x

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MySimApp::Application.config.secret_token = 'dfc1e9781acb650dd273d76ab72af02edb7aafd4585534501c44fef2fdabe4018fa202a942d248de519ce96de53ae4e3418ccf9d595b02f600798b828f16d963'
